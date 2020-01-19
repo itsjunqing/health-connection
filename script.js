b7374255fd7d28660a0af78ecac1c8fb56323eae
Jun Qing Lim <jq3lim@uwaterloo.ca>
2020-01-19 10:15:56 -0500

    linked database with api

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let patientsList = []
 
 class Patient {
-    constructor(name, university, studentId, dateOfBirth, age, registrationDate, condition, info, admissionReason) {
+    constructor(name, studentId, dateOfBirth, age, registrationDate, condition, info, admissionReason) {
         this.name = name;
-        this.university = university;
+        // this.university = university;
         this.studentId = studentId;
         this.dateOfBirth = dateOfBirth;
         this.age = age;
@@ -15,11 +15,23 @@ class Patient {
 }
 $.getJSON("https://v2-api.sheety.co/510691fcb77df6da24df16a1962065ca/starterhacks/students", function(data){
     $.each(data.students, function(property, value){
-        const patient = new Patient(value.name, value.university, value.studentId, value.dateOfBirth, 
-            value.age, value.time, value.registrationDate, value.condition, value.info, value.admissionReason);
+        const patient = new Patient(value.name, value.studentId, value.dateOfBirth, 
+            value.agee, value.registrationDate, value.condition, value.info, value.admissionReason);
         patientsList.push(patient);
+
+        $("tbody").append("<tr><td>" + 
+                        value.name + "</td><td>" + 
+                        value.studentId + "</td><td>" +
+                        value.dateOfBirth + "</td><td>" +
+                        value.age + "</td><td>" +
+                        value.registrationDate + "</td><td>" +
+                        value.condition + "</td><td>" +
+                        value.info + "</td><td>" +
+                        value.admissionReason + "</td></tr>")
     })
 })
+
+console.log(patientsList)
 console.log("testing");
 // request.send();
 
